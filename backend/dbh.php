fa8158ec2930b598e3fc03c0f24a1b47a20fd30f
titu3e <75441242+titu3e@users.noreply.github.com>
2022-02-10 10:53:03 +0530

    Update dbh.php

--- backend/dbh.php
+++ backend/dbh.php
@@ -1,6 +1,7 @@
 <?php
   $mysqli = new mysqli("ec2-54-87-112-29.compute-1.amazonaws.com","exgyzwldvsmlsd","ff712ac1d2213922f40be7a01167249fa7face3757ab2dcd095fbed0949ef00d","d8mad0m2evcfki");
-  if(! $conn ) {
-      die('Could not connect: ' . mysqli_error());
-   }
+ if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
 ?>
