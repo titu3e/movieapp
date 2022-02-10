96dd532645ef4d65882d24a4cd4a8c7463f7c074
titu3e <75441242+titu3e@users.noreply.github.com>
2022-02-10 10:47:57 +0530

    Update dbh.php

--- backend/dbh.php
+++ backend/dbh.php
@@ -1,6 +1,5 @@
 <?php
-  $conn = mysqli_connect("ec2-54-87-112-29.compute-1.amazonaws.com","d8mad0m2evcfki","exgyzwldvsmlsd","
-ff712ac1d2213922f40be7a01167249fa7face3757ab2dcd095fbed0949ef00d");
+  $conn = mysqli_connect("ec2-54-87-112-29.compute-1.amazonaws.com","exgyzwldvsmlsd","ff712ac1d2213922f40be7a01167249fa7face3757ab2dcd095fbed0949ef00d","d8mad0m2evcfki");
   if(! $conn ) {
       die('Could not connect: ' . mysqli_error());
    }
